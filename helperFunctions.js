838fb49e59fe1d9d9d4a884fb291797c97535032
Danielle Knudson <danielle@edmodo.com>
2015-04-17 14:56:36 -0700

    Refactored code, moved into helperFunctions.js, and click events not working after I click on user's name. Need to debug.

---
+++ helperFunctions.js
@@ -0,0 +1,60 @@
+var createElement = function(type, innerContent, classes, id, data){
+	// create jquery element object
+	var el = $('<' + type + '></' + type + '>');
+
+	// add classes to the element
+	for (var i = 0; i < classes.length; i++) {
+		el.addClass(classes[i]);
+	}
+
+	// add ID to the element if any
+	if (id) {
+		el.attr("id", id);
+	}
+
+	// put content into element
+	if (Array.isArray(innerContent) === true) {
+		for (var i = 0; i < innerContent.length; i++) {
+			el.append(innerContent[i]);
+		}
+	} else {
+		el.text(innerContent);
+	}
+
+	return el;
+
+}; // end of createElement
+
+function refreshStream(){
+	// clear feed
+	var feed = $('#feed');
+	feed.html('');
+
+	for (var i = streams.home.length -1; i >= 0; i--){
+		// create tweet to append to stream
+		// create element contains a link, a span, and a paragraph
+		var link = createElement('a', streams.home[i].user, ["link"], streams.home[i].user);
+		var span = createElement('span', jQuery.format.prettyDate(streams.home[i].created_at), ["timestamp"]);
+		var message = createElement('p', streams.home[i].message, ["message"]);
+		var tweet = createElement('div', [link, span, message], ["tweet"]);
+
+		tweet.appendTo(feed);
+	} // end of for loop
+} // end of refreshStream
+
+displayUsersTweets = function(user){
+	
+	var userFeed = $('#user-feed');
+	userFeed.html('<a class="link" id="close-user-feed">Hide User\'s Tweets</a>');
+
+	for (var i = streams.users[user].length - 1; i >= 0 ; i--) {
+		var obj = streams.users[user][i];
+		var link = createElement('a', obj.user, ["link"], obj.user);
+		var span = createElement('span', jQuery.format.prettyDate(obj.created_at), ["timestamp"]);
+		var message = createElement('p', obj.message, ["message"]);
+		var tweet = createElement('div', [link, span, message], ["tweet"]);
+
+		tweet.appendTo(userFeed);
+	}
+
+};
